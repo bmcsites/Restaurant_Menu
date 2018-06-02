f169ba63215df03b11bc55c5a753a62340fd8b37
cfir_laptop\cfir <pussyb@walla.com>
2018-06-02 16:04:53 +0300

    add comments

--- mymenu/mymenu.component.js
+++ mymenu/mymenu.component.js
@@ -1,15 +1,18 @@
 class mymenuController {
     constructor ($http) {
-        this.spinner = false;
-        this.mysearch = '';
+        this.spinner = false; // loading spinner
+        this.mysearch = ''; // search result .
         this.url = 'https://mycheck-menus-dev.s3.amazonaws.com/9272/menu_general.json';
         this.http = $http;
-        this.menu = [];
-        this.dbCall();
+        this.menu = []; // menu items .
+        this.dbCall(); // call DB function .
     }
-
+    
+    // DB call to get menu items . 
     dbCall() {
+        // set loading to on .
         this.spinner = true;
+        // use $http to get json with the menu items 
         this.http.get(this.url)
             .then( (response) => {
                 return response.data;
@@ -17,10 +20,9 @@ class mymenuController {
                 this.spinner = false;
                 console.log('error :', err);
             }).then((data) => {
-            data.shift();
-            console.log(data);
-            this.menu =  data;
-            this.spinner = false;
+            data.shift(); // data start with demo object , shift handle that
+            this.menu =  data; // set data from promise to this.menu var
+            this.spinner = false; // stop loader spinner
         });
     };
 }
